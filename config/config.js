49bd6e20eafda20ca0cb4591c99a503131a22122
SZlonghua <1053595189@qq.com>
2019-08-06 00:12:47 +0800

    初始化

---
+++ config/config.js
@@ -0,0 +1,12 @@
+export default {
+    singular: true,//添加 singular 为 true 来让 page 变为约定的文件夹,在 umi 中，约定的存放页面代码的文件夹是 pages
+    plugins: [
+        ['umi-plugin-react', {
+            // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+        }],
+    ],
+    routes: [{
+        path: '/',
+        component: './HelloWorld',
+    }],
+};
